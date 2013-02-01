70cc7a36107e149a8e4811b7b7b6cf7b112a802d
Sigve <sigvebs@gmail.com>
2013-02-01 12:33:45 +0100

    Added example using Armadillo and classes.

---
+++ examples/Armadillo/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "src/mainapplication.h"
+
+using namespace std;
+
+int main()
+{
+    // Documentation for Armadillo: http://arma.sourceforge.net/
+    MainApplication m;
+    m.runApplication();
+
+    return 0;
+}
+
